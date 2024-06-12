62db986e1b61e7a94a6727454109177a580c59be
EricOfficial <eric.naeser@gmx.de>
2024-06-12 15:49:50 +1000

    Cleaned up wallet creation Added .gitignore

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,73 @@
-const { ethers } = require('hardhat')
+const {hre, ethers} = require('hardhat')
+var Table = require('cli-table3')
+var colors = require('@colors/colors');
 
 
 /*Script to interact with all functions of CSPlatform.sol contract*/
 async function main() {
+
+    // Deploy CSPlatform contract
+    console.log("Deploying CSPlatform...");
     const CSPlatformFactory = await ethers.getContractFactory("CSPlatform");
     const CSPlatform = await CSPlatformFactory.deploy();
     await CSPlatform.waitForDeployment();
+    console.log("CSPlatform deployed to:", await CSPlatform.getAddress());
+    console.log(" ");
 
-    const [account1, account2] = await ethers.getSigners();
+    // Set variables
     const imageUrl = "http://example.com"; // Replace with actual image URL
-    console.log("Account 1 address: ", account1.address);
-    console.log("Account 2 address: ", account2.address);
+    const [signer] = await ethers.getSigners();
+    const provider = ethers.provider;
+    let connectedWallets = [];
 
-    const createContributionResponse1 = await CSPlatform.connect(account1).createContribution(imageUrl);
+    // Create 5 wallets and send 10 ETH to each wallet
+    console.log("Create wallets...");
+    var table = new Table({
+        head: [colors.blue('Wallet'), colors.blue('Public Key'), colors.blue('Private Key'), colors.blue('Balance in ETH')]
+    });
+
+    for (let i = 0; i < 5; i++) {
+
+        // Create wallets
+        let wallet = ethers.Wallet.createRandom();
+        let connectedWallet = wallet.connect(provider);
+        let tx = await signer.sendTransaction({to: connectedWallet.address, value: ethers.parseEther("10")});
+        await tx.wait();
+        connectedWallets.push(connectedWallet);
+
+        // Display wallets
+        let walletName = "Wallet " + (i + 1);
+        let walletAddress = connectedWallet.address;
+        let wallletPrivateKey = connectedWallet.privateKey;
+        let walletBalance = await provider.getBalance(connectedWallet.address);
+        table.push([walletName, walletAddress, wallletPrivateKey, ethers.formatEther(walletBalance)]);
+
+    }
+
+    console.log(table.toString());
+    console.log("Wallets created!");
+    console.log(" ");
+
+    const createContributionResponse1 = await CSPlatform.connect(connectedWallets[0]).createContribution(imageUrl);
     const createContributionReceipt1 = await createContributionResponse1.wait();
     const [createContributionEvent1] = createContributionReceipt1.logs;
     const [contributionParticipant1, contributionImage1, contributionId1] = createContributionEvent1.args;
     console.log("Contribution created: ", contributionParticipant1, contributionImage1, contributionId1.toString());
 
-    const createContributionResponse2 = await CSPlatform.connect(account1).createContribution(imageUrl);
+    const createContributionResponse2 = await CSPlatform.connect(connectedWallets[0]).createContribution(imageUrl);
     const createContributionReceipt2 = await createContributionResponse2.wait();
     const [createContributionEvent2] = createContributionReceipt2.logs;
     const [contributionParticipant2, contributionImage2, contributionId2] = createContributionEvent2.args;
     console.log("Contribution created: ", contributionParticipant2, contributionImage2, contributionId2.toString());
 
-    const createContributionResponse3 = await CSPlatform.connect(account1).createContribution(imageUrl);
+    const createContributionResponse3 = await CSPlatform.connect(connectedWallets[0]).createContribution(imageUrl);
     const createContributionReceipt3 = await createContributionResponse3.wait();
     const [createContributionEvent3] = createContributionReceipt3.logs;
     const [contributionParticipant3, contributionImage3, contributionId3] = createContributionEvent3.args;
     console.log("Contribution created: ", contributionParticipant3, contributionImage3, contributionId3.toString());
 
     // Call assignContribution function with account2
-    const assignContributionResponse = await CSPlatform.connect(account2).assignContribution();
+    const assignContributionResponse = await CSPlatform.connect(connectedWallets[1]).assignContribution();
     const assignContributionReceipt = await assignContributionResponse.wait();
     const [assignContributionEvent] = assignContributionReceipt.logs;
     const [contributionParticipant, contributionImage, contributionId, contributionReviewer] = assignContributionEvent.args;
